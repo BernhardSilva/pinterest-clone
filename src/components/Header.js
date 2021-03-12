a1a1954110f8be2a287fdc18595c55667b542852
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-12 10:46:46 -0300

    Header features and Mainboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PinterestIcon from '@material-ui/icons/Pinterest';
 import IconButton from '@material-ui/core/IconButton';
@@ -7,162 +7,177 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import TextsmsIcon from '@material-ui/icons/Textsms';
 import FaceIcon from '@material-ui/icons/Face';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-
+import { makeStyles } from '@material-ui/core';
 
 // props: {
 //     onSubmit: onSearchSubmit()
 // }
 
-
 function Header(props) {
-    const[input, setInput] = useState("");
-    
-    const onSearchSubmit = (e) => {
-        e.preventDefault();
-        props.onSubmit(input);
-        console.log("this is the input (Header) =>", input);
-    }
+	const [input, setInput] = useState('');
 
-    return (
-        <Wrapper>
-            <LogoWrapper>
-                <IconButton>
-                    <PinterestIcon/>
-                </IconButton>
+	const onSearchSubmit = (e) => {
+		e.preventDefault();
+		props.onSubmit(input);
+		// console.log('this is the input (Header) =>', input);
+	};
 
-            </LogoWrapper>
+	return (
+		<Wrapper>
+			<LogoWrapper>
+				<IconButton>
+					<PinterestIcon />
+				</IconButton>
+			</LogoWrapper>
 
-            <HomePageButton>
-                <a href="/">Homepage</a>
-            </HomePageButton>
+			<HomePageButton>
+				<a href="/">Homepage</a>
+			</HomePageButton>
 
-            <FollowingButton>
-                <a href="/">Following</a>
+			<FollowingButton>
+				<a href="/">Following</a>
+			</FollowingButton>
+			<SearchWrapper>
+				<SearchBarWrapper>
+					<IconButton>{/* <SearchIcon /> */}</IconButton>
+					<form>
+						{/* <input type="text" onChange={(e) => console.log(e.target.value)}/> */}
+						<input
+							type="text"
+							onChange={(e) => setInput(e.target.value)}
+							placeholder="Search"
+						/>
+						<button
+							className="onSearchButton"
+							type="submit"
+							onClick={onSearchSubmit}
+						/>
+					</form>
+				</SearchBarWrapper>
+			</SearchWrapper>
 
-            </FollowingButton>
-            <SearchWrapper>
-                <SearchBarWrapper>
-                <IconButton>
-                    <SearchIcon onClick={onSearchSubmit}/>
-                </IconButton>
-                    <form>
-                        {/* <input type="text" onChange={(e) => console.log(e.target.value)}/> */}
-                        <input type="text" onChange={(e) => setInput(e.target.value)} placeholder="Search"/>
-                        <button type="submit" onClick={onSearchSubmit}/>
-                    </form>
-                </SearchBarWrapper>
-            </SearchWrapper>
-        
-        <IconsWrapper>
-            <IconButton>
-                <NotificationsIcon />
-            </IconButton>
-            <IconButton>
-                <TextsmsIcon />
-            </IconButton>
-            <IconButton>
-                <FaceIcon />
-            </IconButton>
-            <IconButton>
-                <KeyboardArrowDownIcon />
-            </IconButton>
-
-        </IconsWrapper>
-        </Wrapper>
-    )
+			<IconsWrapper>
+				<IconButton>
+					<NotificationsIcon />
+				</IconButton>
+				<IconButton>
+					<TextsmsIcon />
+				</IconButton>
+				<IconButton>
+					<FaceIcon />
+				</IconButton>
+				<IconButton>
+					<KeyboardArrowDownIcon />
+				</IconButton>
+			</IconsWrapper>
+		</Wrapper>
+	);
 }
 
-export default Header
-
+export default Header;
 
 //Header Component Style
 
 const WrapperCore = styled.div`
-align-items: center;
-padding: 12px 4px 4px 16px;
-`
+	align-items: center;
+	padding: 12px 4px 4px 16px;
+`;
 
 const Wrapper = styled(WrapperCore)`
-    display: flex;
-    height: 56px;
-    background-color: white;
-    color: black;
-`
+	display: flex;
+	height: 56px;
+	background-color: white;
+	color: black;
+`;
 const LogoWrapper = styled.div`
-    .MuiSvgIcon-root {
-        color: #e60023;
-        font-size: 32px;
-        cursor: pointer; 
-    }
-`
+	.MuiSvgIcon-root {
+		color: #e60023;
+		font-size: 32px;
+		cursor: pointer;
+	}
+`;
 const HomeButtons = styled.div`
-    display: flex;
-    height: 48px;
-    min-width: 123px;
-    align-items: center;
-    justify-content: center;
-    border-radius: 24px;
-    cursor: pointer;
-`
+	display: flex;
+	height: 48px;
+	min-width: 80px;
+	align-items: center;
+	justify-content: center;
+	border-radius: 24px;
+	cursor: pointer;
+`;
 const HomePageButton = styled(HomeButtons)`
-    background-color: rgb(17, 17, 17);
+	background-color: rgb(17, 17, 17);
 
-    a {
-        text-decoration: none;
-        color: white;
-        font-weight: 700;
-    }
-`
+	a {
+		text-decoration: none;
+		color: white;
+		font-weight: 700;
+	}
+`;
 
 const FollowingButton = styled(HomeButtons)`
-    background-color: white;
+	background-color: white;
 
-    a {
-        text-decoration: none;
-        color: black;
-        font-weight: 700;
-    }
+	a {
+		text-decoration: none;
+		color: black;
+		font-weight: 700;
+	}
 
-    :hover {
-        background-color: #e1e1e1;
-    }
-`
+	:hover {
+		background-color: #e1e1e1;
+	}
+`;
+
+// const styles = theme => ({
+// 	root: {
+// 	  padding: theme.spacing(1),
+// 	  [theme.breakpoints.down('sm')]: {
+// 		HomePageButton: theme.palette.secondary.main,
+// 	  },
+// 	  [theme.breakpoints.up('md')]: {
+// 		backgroundColor: theme.palette.primary.main,
+// 	  },
+// 	  [theme.breakpoints.up('lg')]: {
+// 		backgroundColor: green[500],
+// 	  },
+// 	},
+//   });
 
 const SearchWrapper = styled.div`
-    flex: 1;
-`
+	flex: 1;
+`;
 
 const SearchBarWrapper = styled.div`
-    background-color: #efefef;
-    display: flex;
-    height: 48px;
-    width: 100%;
-    border-radius: 50px;
-    border: none;
-    padding-left: 10px;
+	background-color: #efefef;
+	display: flex;
+	height: 48px;
+	width: 100%;
+	border-radius: 50px;
+	border: none;
+	padding-left: 10px;
 
-    form {
-        display: flex;
-        flex: 1;
-    }
+	form {
+		display: flex;
+		flex: 1;
+	}
 
-    form > input {
-        background-color: transparent;
-        border: none;
-        width: 100%;
-        margin-left: 5px;
-        font-size: 16px;
-    }
+	form > input {
+		background-color: transparent;
+		border: none;
+		width: 100%;
+		margin-left: 5px;
+		font-size: 16px;
+	}
 
-    //Hide the submit button, because it's useless here.
-    form > button {
-        display: none;
-    }
-    
-    //Delete the ugly square around the input
-    input: focus {
-        outline: none;
-    }
-`
-const IconsWrapper = styled(WrapperCore)`
-`
+	//Hide the submit button, because it's useless here.
+	form > button {
+		display: none;
+	}
+
+	//Delete the ugly square around the input
+	input: focus {
+		outline: none;
+	}
+`;
+const IconsWrapper = styled(WrapperCore)``;
--- src/components/Mainboard.js
+++ src/components/Mainboard.js
@@ -4,41 +4,39 @@ import styled from 'styled-components';
 import Pin from './Pin';
 
 function Mainboard(props) {
+	let { pins } = props;
 
-    let { pins } = props;
-
-    return (
-        <Wrapper>
-            <Container className="mainboard__container">
-                {pins.map((pin, index) => { 
-                    let {urls} = pin;   
-                    //Use the key prop when Rendering a List with React            
-                    return <Pin key={index} urls={urls}/>
-                })}
-            </Container>
-        </Wrapper>
-    )
+	return (
+		<Wrapper>
+			<Container className="mainboard__container">
+				{pins.map((pin, index) => {
+					let { urls } = pin;
+					//Use the key prop when Rendering a List with React
+					return <Pin key={index} urls={urls} />;
+				})}
+			</Container>
+		</Wrapper>
+	);
 }
 
 export default Mainboard;
 
-
 // MainBoard Component Style
 
 const Wrapper = styled.div`
-    background-color: white;
-    display: flex;
-    width: 100%;
-    height: 100%;
-    margin-top: 15px;
-    justify-content: center;
-`
+	background-color: white;
+	display: flex;
+	width: 100%;
+	height: 100%;
+	margin-top: 15px;
+	justify-content: center;
+`;
 
 const Container = styled.div`
-    //display: flex-wrap;
-    //column-count: 5;
-    //column-gap: 10px;
-    margin: 0 auto;
-    height: 100%;
-    background-color: white;
-`
+	//display: flex-wrap;
+	//column-count: 5;
+	//column-gap: 10px;
+	margin: 0 auto;
+	height: 100%;
+	background-color: white;
+`;
