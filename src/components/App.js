0173818c35460e3ce9f74ff99f1ef17bfead2401
Bernhard Silva <bernhard.silva@gmail.com>
2021-03-17 04:17:13 -0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -5,66 +5,66 @@ import Mainboard from './Mainboard';
 import unsplash from '../api/unsplash';
 
 function App() {
-	const [pins, setNewPins] = useState([]);
+  const [pins, setNewPins] = useState([]);
 
-	//Get images for Pins with Axios
-	const getImages = (term) => {
-		return unsplash.get('https://api.unsplash.com/search/photos', {
-			params: {
-				query: term,
-			},
-		});
-	};
+  //Get images for Pins with Axios
+  const getImages = (term) => {
+    return unsplash.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+    });
+  };
 
-	//When press enter find new Pins
-	const OnSearchSubmit = (term) => {
-		// console.log("on search submit (App) =>", term);
-		getImages(term).then((res) => {
-			let results = res.data.results;
+  //When press enter find new Pins
+  const OnSearchSubmit = (term) => {
+    // console.log("on search submit (App) =>", term);
+    getImages(term).then((res) => {
+      let results = res.data.results;
 
-			let newPins = [...results, ...pins];
-			//Random Pins, like Pinterest algorithm
-			newPins.sort(function (a, b) {
-				return 0.5 - Math.random();
-			});
-			setNewPins(newPins);
-		});
-	};
+      let newPins = [...results, ...pins];
+      //Random Pins, like Pinterest algorithm
+      newPins.sort(function (a, b) {
+        return 0.5 - Math.random();
+      });
+      setNewPins(newPins);
+    });
+  };
 
-	//Random Pins Function, when the web app is starting Boom!
-	const getNewPins = () => {
-		let promises = [];
-		let pinData = [];
+  //Random Pins Function, when the web app is starting Boom!
+  const getNewPins = () => {
+    let promises = [];
+    let pinData = [];
 
-		let pins = ['Programming', 'Code', 'Video Games'];
-		pins.forEach((pinTerm) => {
-			promises.push(
-				getImages(pinTerm).then((res) => {
-					let results = res.data.results;
-					// console.log(results);
+    let pins = ['Programming', 'Code', 'Video Games'];
+    pins.forEach((pinTerm) => {
+      promises.push(
+        getImages(pinTerm).then((res) => {
+          let results = res.data.results;
+          // console.log(results);
 
-					pinData = pinData.concat(results);
-					pinData.sort(function (a, b) {
-						return 0.5 - Math.random();
-					});
-				}),
-			);
-		});
-		Promise.all(promises).then(() => {
-			setNewPins(pinData);
-		});
-	};
+          pinData = pinData.concat(results);
+          pinData.sort(function (a, b) {
+            return 0.5 - Math.random();
+          });
+        }),
+      );
+    });
+    Promise.all(promises).then(() => {
+      setNewPins(pinData);
+    });
+  };
 
-	useEffect(() => {
-		getNewPins();
-	}, []);
+  useEffect(() => {
+    getNewPins();
+  }, []);
 
-	return (
-		<div className="app">
-			<Header onSubmit={OnSearchSubmit} />
-			<Mainboard pins={pins} />
-		</div>
-	);
+  return (
+    <div className="app">
+      <Header onSubmit={OnSearchSubmit} />
+      <Mainboard pins={pins} />
+    </div>
+  );
 }
 
 export default App;
